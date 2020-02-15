31ff865a6f33e8a43416e583d8c3cb308187c23a
AmrutaGhanwat <59525726+AmrutaGhanwat@users.noreply.github.com>
2020-02-15 17:08:13 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Login extends HttpServlet {
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String user = request.getParameter("user");
+        String pass = request.getParameter("pass");
+        
+       if(user.equals("amruta")&&pass.equals("amruta"))
+        {
+            RequestDispatcher rs = request.getRequestDispatcher("Welcome");
+            rs.forward(request, response);
+        }
+        else
+        {
+           out.println("Username or Password incorrect");
+           RequestDispatcher rs = request.getRequestDispatcher("index.html");
+           rs.include(request, response);
+        }
+    }  
+}
